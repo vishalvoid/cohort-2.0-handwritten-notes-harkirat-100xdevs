de3987a2c2fa1f46bf5b77c871e6cdc9394bb42f
Vishal Kumar Singh <kvishal07860@gmail.com>
2025-01-12 02:55:50 +0530

    Update Node and Express notes for clarity; modify examples to use POST method and include body parsing details

--- Practice/Week_2/index.js
+++ Practice/Week_2/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json())
 
 const port = 3000;
 
 app.post("/", (req, res) => {
-  console.log(req.headers);
+  console.log(req.body);
   res.send("hello world");
 });
 
