474cb18c5b1691efb37c320bcadb3e1abc27ed19
Vishal Kumar Singh <kvishal07860@gmail.com>
2025-01-23 13:34:15 +0530

    Add initial testing component with useMemo for performance optimization

--- testing.js
+++ testing.js
@@ -1,1 +1,21 @@
+import React, { useEffect } from "react";
 
+const testing = () => {
+  const [counter, SetCounter] = useState(0);
+  const [inputValue, setInputValue] = useState(1);
+  // const [count, setCount] = useState(0) just to use in useEffect
+
+  // using useMemo
+  let count = useMemo(() => {
+    let finalCount = 0;
+    for (let i = 1; i <= inputValue; i++) {
+      finalCount = finalCount + 1;
+    }
+    return finalCount;
+  }, [inputValue]);
+
+// 
+  return <div></div>;
+};
+
+export default testing;
