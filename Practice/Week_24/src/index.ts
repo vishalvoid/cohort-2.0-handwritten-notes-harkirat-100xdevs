9a6d26cef6164b73355f9eaa7d9e4f49ce023cb6
Vishal Kumar Singh <kvishal07860@gmail.com>
2025-03-27 17:58:01 +0530

    feat: regenerate TypeScript definitions for AddressBookService, GetPersonByNameRequest, and Person

--- Practice/Week_24/src/index.ts
+++ Practice/Week_24/src/index.ts
@@ -2,10 +2,11 @@ import path from "path";
 import * as grpc from "@grpc/grpc-js";
 import { GrpcObject, ServiceClientConstructor } from "@grpc/grpc-js";
 import * as protoLoader from "@grpc/proto-loader";
+import { ProtoGrpcType } from "./generated/a";
 
 const packageDefinition = protoLoader.loadSync(path.join(__dirname, "./a.proto"));
 
-const personProto = grpc.loadPackageDefinition(packageDefinition);
+const personProto = grpc.loadPackageDefinition(packageDefinition) as unknown as ProtoGrpcType;
 
 const PERSONS = [
   {
@@ -18,7 +19,6 @@ const PERSONS = [
   },
 ];
 
-//@ts-ignore
 function addPerson(call, callback) {
   console.log(call);
   let person = {
